5ed78ecc7c4f4454d5dfc08da195eb53adc009f9
Berke Gökmen <berkegokmen1@Berke-MacBook-Pro.local>
2021-12-07 11:48:05 +0300

    Background Filler & Post processing

--- core/src/com/cornichon/utils/LevelReader.java
+++ core/src/com/cornichon/utils/LevelReader.java
@@ -7,45 +7,47 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 import com.cornichon.models.construction.Level;
-import com.cornichon.models.construction.background.BackgroundBrick;
 import com.cornichon.models.construction.components.Brick;
 import com.cornichon.models.entities.aliveEntities.Player;
 import com.cornichon.models.entities.aliveEntities.Skeleton;
 import com.cornichon.views.helpers.DrawableValues;
 import com.cornichon.views.helpers.ScreenDrawable;
 
-public class LevelReader {
+public final class LevelReader {
+
+  private static JsonReader json = new JsonReader();
+
+  public static JsonValue readJsonFile(String fileString) {
+    FileHandle file = Gdx.files.internal("levels/" + fileString);
+    JsonValue value = LevelReader.json.parse(file);
+
+    return value;
+  }
 
   public static Array<ScreenDrawable> readLevel(String levelFile, Level level) {
     final Array<ScreenDrawable> drawables = new Array<ScreenDrawable>();
 
     try {
-      FileHandle file = Gdx.files.internal("levels/" + levelFile);
-      JsonReader json = new JsonReader();
+      JsonValue levelJson = LevelReader.readJsonFile(levelFile);
 
-      JsonValue arr = json.parse(file);
-
-      for (int y = 0; y < arr.size; y += 1) {
-        for (int x = 0; x < arr.get(0).size; x += 1) {
-          // arr[0] 0 => (100)
-          switch (arr.get(y).get(x).asInt()) {
+      for (int y = 0; y < levelJson.size; y += 1) {
+        for (int x = 0; x < levelJson.get(0).size; x += 1) {
+          switch (levelJson.get(y).get(x).asInt()) {
             case DrawableValues.BRICK:
-              drawables.add(new Brick(new Vector2(x, arr.size - y - 1)));
+            case DrawableValues.BRICK_PLATFORM:
+              drawables.add(new Brick(new Vector2(x, levelJson.size - y - 1)));
               break;
             case DrawableValues.PLAYER:
-              Player player = new Player(new Vector2(x, arr.size - y - 1));
+              Player player = new Player(new Vector2(x, levelJson.size - y - 1));
               drawables.add(player);
               level.setPlayer(player);
               break;
             case DrawableValues.SKELETON:
-              drawables.add(new Skeleton(new Vector2(x, arr.size - y - 1)));
+              drawables.add(new Skeleton(new Vector2(x, levelJson.size - y - 1)));
               // Maybe add to the mob array in level to use later?
               break;
             case DrawableValues.BRICK_BACKGROUND:
-              drawables.add(
-                new BackgroundBrick(new Vector2(x, arr.size - y - 1))
-              );
-              // Maybe add to the mob array in level to use later?
+              LevelWriter.fillBackground(levelJson, drawables, x, y);
               break;
           }
         }
